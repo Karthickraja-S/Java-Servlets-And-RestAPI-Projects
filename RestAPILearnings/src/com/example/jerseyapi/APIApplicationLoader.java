be7adaf09918ff46ec676a3cf2ede6f6902cddc3
Karthick Raja <60180945+Karthickraja-S@users.noreply.github.com>
2025-01-05 17:38:52 +0530

    Update APIApplicationLoader.java

--- RestAPILearnings/src/com/example/jerseyapi/APIApplicationLoader.java
+++ RestAPILearnings/src/com/example/jerseyapi/APIApplicationLoader.java
@@ -8,7 +8,7 @@ public class APIApplicationLoader extends Application {
     public Set<Class<?>> getClasses() {
         Set<Class<?>> classSet = new HashSet<>();
         // add Rest API classes below
-        classSet.add(com.example.jerseyapi.extendedAPI.HelloExtnAPI.class);
+        classSet.add(com.example.jerseyapi.extendedAPI.LockAPI.class);
         // all the class registered ( apis ) will also be sent to this filters
         classSet.add(com.example.filters.AnnotationFilter.class);
 
