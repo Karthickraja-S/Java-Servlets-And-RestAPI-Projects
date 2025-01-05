0776753b38fa7621dc2ca9dcb34548dcda33d7c9
Karthick Raja <60180945+Karthickraja-S@users.noreply.github.com>
2025-01-05 19:02:35 +0530

    Update HelloExtnAPI.java

--- RestAPILearnings/src/com/example/jerseyapi/extendedAPI/HelloExtnAPI.java
+++ RestAPILearnings/src/com/example/jerseyapi/extendedAPI/HelloExtnAPI.java
@@ -4,7 +4,6 @@ import com.example.model.LockAPI;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -22,15 +21,16 @@ public class HelloExtnAPI {
     @Produces(MediaType.TEXT_HTML)
     @LockAPI
     public String premiumAPIEx() {
-        return "This is a premium API which can be used only one time. Come back after 60 seconds to access the API.";
+        return "<h1> This is a premium API which can be used for 5 time within 60s. " +
+                "Come back after 60 seconds to access the API. </h1>";
     }
 
     @GET
     @Path("/getV2/premium2")
     @Produces(MediaType.TEXT_HTML)
-    @LockAPI(lockSeconds = 30)
+    @LockAPI(lockSeconds = 30 , apiAllowedToExecute = 3)
     public String premiumAPIEx2() {
-        return "This is a premium 2 API which can be used only one time. Come back after 30 seconds to access the API.";
+        return "<h1> This is a premium2 API which can be used only 3 time. Come back after 30 seconds to access the API. </h1>";
     }
 
 }
