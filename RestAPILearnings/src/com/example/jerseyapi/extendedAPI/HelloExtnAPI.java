f98a732cbf30c5e251db4c4b0e083a49087a57c7
Karthick Raja <60180945+Karthickraja-S@users.noreply.github.com>
2025-01-05 17:31:52 +0530

    Update HelloExtnAPI.java

--- RestAPILearnings/src/com/example/jerseyapi/extendedAPI/HelloExtnAPI.java
+++ RestAPILearnings/src/com/example/jerseyapi/extendedAPI/HelloExtnAPI.java
@@ -1,7 +1,10 @@
 package com.example.jerseyapi.extendedAPI;
 
+import com.example.model.PremiumOnly;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -13,4 +16,21 @@ public class HelloExtnAPI {
     public String getStringV2() {
         return "Getting response from getString() method of jersey version2 API";
     }
+
+    @GET
+    @Path("/getV2/premium")
+    @Produces(MediaType.TEXT_HTML)
+    @PremiumOnly
+    public String premiumAPIEx() {
+        return "This is a premium API which can be used only one time. Come back after 60 seconds to access the API.";
+    }
+
+    @GET
+    @Path("/getV2/premium2")
+    @Produces(MediaType.TEXT_HTML)
+    @PremiumOnly(lockSeconds = 30)
+    public String premiumAPIEx2() {
+        return "This is a premium 2 API which can be used only one time. Come back after 30 seconds to access the API.";
+    }
+
 }
